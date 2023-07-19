c997b9cbf8a5334132d812dcec03c1b2f4e5afe0
jashleymasonite <jashley@masonite.com>
2023-07-19 09:52:02 -0400

    Added in responsiveness for the app

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -30,6 +30,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+        useSafeArea: true,
         isScrollControlled: true,
         context: context,
         builder: (ctx) {
@@ -64,6 +65,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = (MediaQuery.of(context).size.width);
+
     Widget mainContent =
         const Center(child: Text('No expenses found. Start adding some!'));
 
@@ -84,10 +87,16 @@ class _ExpensesState extends State<Expenses> {
             )
           ],
         ),
-        body: Column(
+        body: width < 600 ? Column(
             children: [
               Chart(expenses: _registeredExpenses,),
               Expanded(child: mainContent)
-        ]));
+        ]) : Row(children: [
+          Expanded(
+            child: Chart(expenses: _registeredExpenses,)
+          ),
+          Expanded(child: mainContent)
+        ])
+    );
   }
 }
